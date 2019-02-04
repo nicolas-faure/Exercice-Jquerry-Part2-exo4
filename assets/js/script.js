b01b7919587eb98abcf5044787080f39cc41c2fa
faure <nicolas.faure201@gmail.com>
2019-02-04 11:26:22 +0100

    exo4

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+  $( "#green" ).click(function() {
+    //change color on click green
+  $('#text').css('color','green');
+  });
+  $( "#red" ).click(function() {
+    //change color on click red
+  $('#text').css('color','red');
+  });
+  $( "#blue" ).click(function() {
+    //change color on click blue
+  $('#text').css('color','blue');
+  });
+});
